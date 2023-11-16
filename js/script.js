3e00a212881d0be99e48dfc64ce9cfecc43515c6
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-11-16 18:09:40 -0300

    iniciando-header

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// HEADER //
+  const SvgAccount = document.getElementById("account");
+  const ContainerAccount = document.getElementById("container-account")
+
+
+  SvgAccount.addEventListener("click" , function(){
+ContainerAccount.style.display=("flex")
+
+  })
