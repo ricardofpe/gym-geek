0c1165a98a50849051bd0bc2db6398295ef5c3c9
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-01-19 16:59:37 -0300

    doing-node.js-login-resgister

--- js/app.js
+++ js/app.js
@@ -13,9 +13,46 @@ const app = express()
 
 app.use(express.json())
 
+
+//Open Route - Public Route
 app.get('/', (req, res)=>{
     res.status(200).json({msg:'bem vindo a api'})
 })
+
+//Private Route
+
+app.get("/user/:id",checkToken, async(req,res) =>{
+    const id = req.params.id
+
+    //check if user exists
+
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({msg:'Usuário não encontrado!'})
+    }
+
+    res.status(200).json({user})
+
+})
+
+function checkToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({msg:'Acesso megad0!'})
+    } try{
+
+        const secret = process.env.SECRET
+        jwt.verify(token,secret)
+
+        next()
+
+    }catch(error){
+        res.status(400).json({msg:"Token inválido!"})
+    }
+}
 
 //Models
 
@@ -80,6 +117,57 @@ app.post('/auth/register', async(req,res) =>{
     })
     }
 })
+
+//login User
+
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+
+//validations
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email é obrigatório' });
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatória' });
+    }
+
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado!' });
+    }
+
+  //check password
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+        return res.status(422).json({ msg: 'Senha inválida!' });
+    }
+
+    try {
+       
+const secret = process.env.SECRET
+
+const token = jwt.sign(
+    {
+        id: user._id,
+    },
+    secret,
+)
+
+res.status(200).json({msg:'Autentificação realizada com sucesso', token})
+        
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({ msg: 'Aconteceu um erro no servidor!' });
+    }
+});
+
 
 //Credencials
 
